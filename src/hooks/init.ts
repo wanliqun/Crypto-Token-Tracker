aca44afa577384809dc21f3bb536351aa89684d0
Conflux <conflux@ConfluxdeMBP-3.lan>
2023-08-07 18:52:58 +0800

    update

--- src/hooks/init.ts
+++ src/hooks/init.ts
@@ -1,5 +1,6 @@
 import {Hook} from '@oclif/core'
 import {registeredShutdowns} from '../util/graceful'
+import * as heapdump from "heapdump"
 
 export const hook: Hook<'init'> = async function (options) {
   const startGracefulShutdown = async () => {
@@ -13,7 +14,12 @@ export const hook: Hook<'init'> = async function (options) {
   process.on('SIGTERM', () => {
     startGracefulShutdown()
   })
+  
   process.on('SIGINT', () => {
     startGracefulShutdown()
   })
+
+  process.on("SIGUSR2", function() {
+    heapdump.writeSnapshot(Date.now() + '.heapsnapshot');
+  });
 }
